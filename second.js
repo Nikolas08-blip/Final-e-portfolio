476158bd33210d73de3076aa4dd501842c5336eb
Nikolas08-blip <158699723+Nikolas08-blip@users.noreply.github.com>
2025-10-05 14:23:39 +0800

    Add files via upload

---
+++ second.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_n6x6d57","template_zc9g1tg",parms).then(alert("Email Sent."))
+}
